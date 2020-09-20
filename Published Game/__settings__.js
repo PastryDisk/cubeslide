09d66aa4db8dd8d00167e5b6ab1f3d24f72b84a2
PastryDisk <43475824+PastryDisk@users.noreply.github.com>
2020-09-20 03:56:59 -0400

    Final Push

---
+++ Published Game/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "992080.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true
+};
+SCRIPTS = [ 35555544, 35556089, 35556743, 35566606, 35566618, 35566685, 35567366, 35571721, 35571822, 35572384, 35572798, 35575879, 35576147, 35576259 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/35554946/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/35554947/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/35554945/1/ammo.js', 'preload' : true},
+];
